f7f54a3aac7d0b16d2d03b47c8da36cc1e3a7f0e
ThaiThiMyTien <thaithimytien0906@gmail.com>
2021-03-29 07:45:05 +0700

    add config

---
+++ login/config/config.php
@@ -0,0 +1,11 @@
+<?php
+define ('DB_NAME','login');
+define('DB_USER','root');
+define('DB_PASSWORD', '');
+define('DB_HOST', 'localhost');
+define('PORT', '3306');
+define('DB_CHARSET', 'utf8mb4');
+?>
+
+
+
